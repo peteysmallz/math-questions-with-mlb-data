0e7621c0be6de0bc9d51074df577c0288d0baba1
Pete Smaluck <pete@runsandbox.com>
2019-06-02 13:05:50 -0400

    Some code cleanup

--- src/mlb/api.js
+++ src/mlb/api.js
@@ -3,10 +3,8 @@
  * @param   {stat} the hitting stat ie. 'hr', 'avg'
  * @return  {Array} The top hitters by stat
  */
-
- export async function fetchHittingLeaderboardByStat(stat, count) {
+export async function fetchHittingLeaderboardByStat(stat, count) {
   const HITTING_LEADERBOARD_URL = `http://mlb.mlb.com/pubajax/wf/flow/stats.splayer?season=2017&sort_order=%27desc%27&sort_column=%27${stat}%27&stat_type=hitting&page_type=SortablePlayer&game_type=%27R%27&player_pool=QUALIFIER&season_type=ANY&sport_code=%27mlb%27&results=1000&recSP=1&recPP=${count}`;
-  
   const response = await fetch(HITTING_LEADERBOARD_URL);
 
   if (response.status === 200) {
@@ -22,10 +20,8 @@
  * @param  {id} the player's unique id
  * @return {object}
  */
- export async function fetchPlayerInfo(id) {
-  // 514888
+export async function fetchPlayerInfo(id) {
   const PLAYER_DATA_URL = `https://statsapi.mlb.com/api/v1/people/${id}?hydrate=currentTeam,team,stats(type=[yearByYear,yearByYearAdvanced,careerRegularSeason,careerAdvanced,availableStats](team(league)),leagueListId=mlb_hist)&site=en`;
-  
   const response = await fetch(PLAYER_DATA_URL);
 
   if (response.status === 200) {
@@ -41,10 +37,8 @@
  * @param  {id} the player's unique id
  * @return {object}
  */
- export async function fetchPlayerStats(id) {
-  // 514888
+export async function fetchPlayerStats(id) {
   const PLAYER_DATA_URL = `https://statsapi.mlb.com/api/v1/people/${id}?hydrate=currentTeam,team,stats(type=[yearByYear,yearByYearAdvanced,careerRegularSeason,careerAdvanced,availableStats](team(league)),leagueListId=mlb_hist)&site=en`;
-  
   const response = await fetch(PLAYER_DATA_URL);
 
   if (response.status === 200) {
@@ -55,9 +49,13 @@
   }
 }
 
- export async function fetchTeamRoster(id = 141) {
+/**
+ * Returns an array of a players from a team
+ * @param  {id} a team's unique id (defaulting to the Blue Jays)
+ * @return {array}
+ */
+export async function fetchTeamRoster(id = 141) {
   const TEAM_ROSTER_URL = `http://mlb.mlb.com/pubajax/wf/flow/stats.splayer?season=2019&sort_order=%27desc%27&sort_column=%27ab%27&stat_type=hitting&page_type=SortablePlayer&team_id=${id}&game_type=%27R%27&player_pool=ALL&season_type=ANY&sport_code=%27mlb%27&results=1000&recSP=1&recPP=50`;
-  
   const response = await fetch(TEAM_ROSTER_URL);
 
   if (response.status === 200) {
@@ -67,6 +65,3 @@
     return parsedData;
   }
 }
-
-
-// https://statsapi.mlb.com/api/v1/people/514888?hydrate=currentTeam,team,stats(type=[yearByYear,yearByYearAdvanced,careerRegularSeason,careerAdvanced,availableStats](team(league)),leagueListId=mlb_hist)&site=en
--- src/utils/index.js
+++
@@ -1,6 +0,0 @@
-
-export default function getObjectKeysFromArrayOfObjects(array) {
-  return Object.keys(array.reduce(function(result, obj) {
-    return Object.assign(result, obj);
-  }, {}))
-}
